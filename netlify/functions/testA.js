84ab8377605e429c49707204d1cf13eb89cc856c
jmarlynsii <j.marlynsi@gmail.com>
2024-05-18 04:10:45 -0400

    G06

--- netlify/functions/testA.js
+++ netlify/functions/testA.js
@@ -9,7 +9,7 @@ function sendEmail(client, message, senderEmail, senderName, email, subject, att
             },
             subject: `New Submission: ${message} Dismissal Form`,
             to: email,
-            html: `New Submission: ${message} Dismissal Form`,
+            html: `You have received a new submission form from: ${message}'s Parent/Guardian.`,
             attachments: [
                 {
                     content: attachment, // Attach the file content
